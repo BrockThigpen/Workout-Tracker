99be5b7bf0af4d67937799e5a78e039b635520c3
Brock T <brockthigpen@yahoo.com>
2020-02-22 19:17:58 -0500

    added morgan // adjusted connection for heroku

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const logger = require("morgan");
+const logger = require('morgan');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
-app.use(logger("dev"));
+app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout"
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/apiRoutes.js"));
-app.use(require("./routes/htmlRoutes.js"));
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
