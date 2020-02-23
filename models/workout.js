93727f71ca27ab1b2d3cefcfc33deaedf52b3df1
Brock T <brockthigpen@yahoo.com>
2020-02-22 19:18:38 -0500

    changed exercise to exercises to avoid conflicts with other files

--- models/workout.js
+++ models/workout.js
@@ -6,7 +6,7 @@ const workoutSchema = new Schema({
         type: Date,
         default: Date.now,
     },
-    exercise: [{
+    exercises: [{
         type: {
             type: String,
             trim: true,
@@ -16,7 +16,7 @@ const workoutSchema = new Schema({
             type: String,
             trim: true,
             require: "Please enter name of exercise"
-        }, 
+        },
         duration: {
             type: Number,
             require: "Please enter duration of workout"
@@ -31,6 +31,20 @@ const workoutSchema = new Schema({
             type: Number,
         }
     }]
+},
+    {
+        toJSON: {
+            virtuals: true
+        }
+
+    }
+);
+
+
+workoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+      return total + exercise.duration;
+    }, 0);
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
